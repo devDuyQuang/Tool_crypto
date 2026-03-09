c24ae6898815f87c2ea50af1972d2ddd92a9ea62
Vo Duy Quang <voquagduy28@gmail.com>
2026-03-10 04:55:14 +0700

    update: fix something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     const router = useRouter();
 
     useEffect(() => {
-        router.replace("/dashboard");
+        router.replace("/signin");
     }, [router]);
 
     return null;
